876921c95b2b205a8f922c2a6a33d3d759eeffc3
Priyankagit2004 <168293600+Priyankagit2004@users.noreply.github.com>
2026-08-12 16:05:20 +0530

    Add files via upload

---
+++ 12-08-2026/React/ecommapp/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Main from "./components/main/Main";
+import Card from "./components/card/Card";
+function App(){
+  return <div>
+    <Header />
+    <Main />
+    <Main />
+    <Main />
+    <Footer />
+    <Card />
+  </div>
+}
+
+export default App;
