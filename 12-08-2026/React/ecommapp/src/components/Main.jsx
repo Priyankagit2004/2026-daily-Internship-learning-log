430d2cf580bf3225a8230409f41923dc50f08298
Priyankagit2004 <168293600+Priyankagit2004@users.noreply.github.com>
2026-08-12 16:06:51 +0530

    Add files via upload

---
+++ 12-08-2026/React/ecommapp/src/components/Main.jsx
@@ -0,0 +1,10 @@
+import "./Main.css";
+
+function Main(){
+    return <div>
+        <r className="main"> </r>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate natus quos incidunt id, laboriosam quia, quisquam distinctio facilis eos asperiores, quidem provident hic modi. Tenetur!</p>
+    </div>
+}
+
+export default Main;
